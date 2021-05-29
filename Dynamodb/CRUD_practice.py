83217c7ce0e25be6d5ef27a7c82175c98ce4ea24
Msvsr <msvsr11297@gmail.com>
2021-05-29 20:16:16 +0530

    rewriting the scan covering all cases

--- Dynamodb/CRUD_practice.py
+++ Dynamodb/CRUD_practice.py
@@ -8,7 +8,7 @@ DELETE
 """
 
 import boto3
-from boto3.dynamodb.types import TypeDeserializer,TypeSerializer
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
 
 def convert_to_dynamodb_format(data):
@@ -41,8 +41,8 @@ def read_all_the_items_batch_get_item(table, key, attributes=None):
         attributes = []
 
     if attributes:
-        request_items = {table:{'Keys':[convert_to_dynamodb_format(key)['M']],
-                                'ProjectionExpression':",".join(attributes)}}
+        request_items = {table: {'Keys': [convert_to_dynamodb_format(key)['M']],
+                                 'ProjectionExpression': ",".join(attributes)}}
     else:
         request_items = {table: {'Keys': [convert_to_dynamodb_format(key)['M']]}}
 
@@ -53,39 +53,40 @@ def read_all_the_items_batch_get_item(table, key, attributes=None):
     return [convert_to_python_format(item.items()) for item in response.get('Responses').get(table)]
 
 
-def read_all_the_items_scan(table, attributes=None, filter_condition=None, attribute_expression=None,
-                            attribute_names=None):
+def read_all_the_items_scan(table, attributes=None, filter_condition=None, attribute_names=None,
+                            attribute_values=None):
     """Reads all the items from the table using batch_get_item"""
     if attribute_names is None:
         attribute_names = {}
-    if attribute_expression is None:
-        attribute_expression = {}
+    if attribute_values is None:
+        attribute_values = {}
     if filter_condition is None:
         filter_condition = []
     if attributes is None:
         attributes = []
 
-    response = {}
-
     dynamodb = boto3.client('dynamodb')
 
-    if attributes and filter_condition and attribute_expression and attribute_names:
+    if attributes and filter_condition and attribute_names and attribute_values:
         response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
                                  FilterExpression=" and ".join(filter_condition),
-                                 ExpressionAttributeValues=attribute_expression,
-                                 ExpressionAttributeNames=attribute_names)
-    elif attributes and filter_condition and attribute_expression:
-        response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
-                                 FilterExpression=" and ".join(filter_condition),
-                                 ExpressionAttributeValues=attribute_expression)
+                                 ExpressionAttributeNames=attribute_names,
+                                 ExpressionAttributeValues=attribute_values)
+
     elif attributes and filter_condition and attribute_names:
         response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
                                  FilterExpression=" and ".join(filter_condition),
                                  ExpressionAttributeNames=attribute_names)
 
+    elif attributes and filter_condition and attribute_values:
+        response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
+                                 FilterExpression=" and ".join(filter_condition),
+                                 ExpressionAttributeValues=attribute_values)
+
     elif attributes and filter_condition:
         response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
                                  FilterExpression=" and ".join(filter_condition))
+
     elif attributes and attribute_names:
         response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes),
                                  ExpressionAttributeNames=attribute_names)
@@ -94,8 +95,8 @@ def read_all_the_items_scan(table, attributes=None, filter_condition=None, attri
         response = dynamodb.scan(TableName=table, ProjectionExpression=",".join(attributes))
 
     elif filter_condition:
-        response = dynamodb.scan(TableName=table, FilterExpression=" and ".join(filter_condition),
-                                 ExpressionAttributeValues=attribute_expression)
+        response = dynamodb.scan(TableName=table, FilterExpression=" and ".join(filter_condition))
+
     else:
         response = dynamodb.scan(TableName=table)
 
@@ -107,36 +108,75 @@ if __name__ == '__main__':
 
     # read_specific_item
     read_specific_item_key_args = {'StorePlace': 'Gopalapuram', 'StoreID': 1}
-    read_specific_item_attribute_args = ['Products','Employees']
-    # print(read_specific_item(table_name, read_specific_item_key_args, read_specific_item_attribute_args))
-    # print(read_specific_item(table_name, read_specific_item_key_args))
+    read_specific_item_attribute_args = ['Products', 'Employees']
+    data1 = read_specific_item(table_name, read_specific_item_key_args, read_specific_item_attribute_args)
+    print(data1)
+    data2 = read_specific_item(table_name, read_specific_item_key_args)
+    print(data2)
 
     # read_all_the_items_batch_get_item - not useful if table has both partition key and sort key
-    read_all_the_items_key_args = {'StorePlace': 'Gopalapuram','StoreID':1}
+    read_all_the_items_key_args = {'StorePlace': 'Gopalapuram', 'StoreID': 1}
     read_all_the_items_attribute_args = ['Employees']
-    # print(read_all_the_items_batch_get_item(table_name,read_all_the_items_key_args))
-    # print(read_all_the_items_batch_get_item(table_name,read_all_the_items_key_args,read_all_the_items_attribute_args))
+    data3 = read_all_the_items_batch_get_item(table_name, read_all_the_items_key_args)
+    print(data3)
+    data4 = read_all_the_items_batch_get_item(table_name, read_all_the_items_key_args,
+                                              read_all_the_items_attribute_args)
+    print(data4)
 
     # read_all_the_items_scan
-    # read_all_the_items_scan_attribute_args = ['StorePlace','StoreID','Employees']
-    read_all_the_items_scan_attribute_args = ['StorePlace','StoreID']
+    # arguments - table
+    data5 = read_all_the_items_scan(table_name)
+    print(data5)
 
-    # read_all_the_items_scan_filter_args = ['StoreID= :id', 'StorePlace= :place']
-    # converted_dynamodb_format = convert_to_dynamodb_format({':id': 1,':place':'Tanuku'})['M']
+    # arguments - table,filter_condition
+    read_all_the_items_scan_filter_args = ['attribute_exists(Employees.E5)']
+    data6 = read_all_the_items_scan(table_name, filter_condition=read_all_the_items_scan_filter_args)
+    print(data6)
 
+    # arguments - table,attributes
+    read_all_the_items_scan_attributes_args = ['StorePlace', 'StoreID', 'Employees']
+    data7 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attributes_args)
+    print(data7)
+
+    # arguments - table,attributes,attribute_names
+    read_all_the_items_scan_attributes_args = ['Employees.#id', 'Employees.#id1']
+    read_all_the_items_scan_attribute_names_args = {'#id': '2', '#id1': 'E5'}
+    data8 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attributes_args,
+                                    attribute_names=read_all_the_items_scan_attribute_names_args)
+    print(data8)
+
+    # arguments - table,attributes,filter_condition
+    read_all_the_items_scan_attributes_args = ['StorePlace', 'StoreID', 'Employees']
+    read_all_the_items_scan_filter_args = ['attribute_exists(Employees.E5)']
+    data9 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attributes_args,
+                                    filter_condition=read_all_the_items_scan_filter_args)
+    print(data9)
+
+    # arguments = table,attributes,filter_condition,attribute_values
+    read_all_the_items_scan_attribute_args = ['StorePlace', 'StoreID', 'Employees']
+    read_all_the_items_scan_filter_args = ['StoreID= :id', 'StorePlace= :place']
+    read_all_the_items_scan_attribute_value_args = convert_to_dynamodb_format({':id': 1, ':place': 'Tanuku'})['M']
+    data10 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attribute_args,
+                                     filter_condition=read_all_the_items_scan_filter_args,
+                                     attribute_values=read_all_the_items_scan_attribute_value_args)
+    print(data10)
+
+    # arguments = table,attributes,filter_condition,attribute_names
+    read_all_the_items_scan_attribute_args = ['StorePlace', 'StoreID', 'Employees']
     read_all_the_items_scan_filter_args = ['attribute_exists(Employees.#newid)']
-    # converted_dynamodb_format = convert_to_dynamodb_format({})['M']
+    read_all_the_items_scan_attribute_names_args = {"#newid": '2'}
+    data11 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attribute_args,
+                                     filter_condition=read_all_the_items_scan_filter_args,
+                                     attribute_names=read_all_the_items_scan_attribute_names_args)
+    print(data11)
 
-    # read_all_the_items_scan_filter_args = ['contains(Employees.#id.#Name,:name)']
-    read_all_the_items_scan_attribute_names_args = {"#newid":'2'}
-    converted_dynamodb_format = convert_to_dynamodb_format({':name': 'Tata Reddy'})['M']
-
-    read_all_the_items_scan_expression_attribute_values_args = converted_dynamodb_format
-
-    print(read_all_the_items_scan(table_name, read_all_the_items_scan_attribute_args,
-                                  filter_condition=read_all_the_items_scan_filter_args,
-                                  attribute_names=read_all_the_items_scan_attribute_names_args))
-    # print(read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attribute_args))
-    # print(read_all_the_items_scan(table_name,filter_condition=read_all_the_items_scan_filter_args,
-    #                               attribute_expression=read_all_the_items_scan_expression_attribute_values_args))
-    # print(read_all_the_items_scan(table_name))
+    # arguments = table,attributes,filter_condition,attribute_names
+    read_all_the_items_scan_attribute_args = ['StorePlace', 'StoreID', 'Employees']
+    read_all_the_items_scan_filter_args = ['contains(Employees.#newid.#Name,:name)']
+    read_all_the_items_scan_attribute_names_args = {"#newid": '1', '#Name': 'Name'}
+    read_all_the_items_scan_attribute_values_args = convert_to_dynamodb_format({':name': 'Tata Reddy'})['M']
+    data12 = read_all_the_items_scan(table_name, attributes=read_all_the_items_scan_attribute_args,
+                                     filter_condition=read_all_the_items_scan_filter_args,
+                                     attribute_names=read_all_the_items_scan_attribute_names_args,
+                                     attribute_values=read_all_the_items_scan_attribute_values_args)
+    print(data12)
